0ec726583e234280c06ea034a55fc1d3ec9a6de4
TCaldato <>
2024-02-13 12:22:03 +0000

    starting allauth

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -37,6 +38,10 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    'django.contrib.sites',
+    'allauth',
+    'allauth.account',
+    'allauth.socialaccount',
 ]
 
 MIDDLEWARE = [
@@ -47,6 +52,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "allauth.account.middleware.AccountMiddleware",
 ]
 
 ROOT_URLCONF = "boutique_ado.urls"
@@ -59,13 +65,25 @@ TEMPLATES = [
         "OPTIONS": {
             "context_processors": [
                 "django.template.context_processors.debug",
-                "django.template.context_processors.request",
+                "django.template.context_processors.request", # required by allauth
                 "django.contrib.auth.context_processors.auth",
                 "django.contrib.messages.context_processors.messages",
             ],
         },
     },
 ]
+
+AUTHENTICATION_BACKEND = (
+
+    # Needed to login by username in Django admin, regardless of allauth
+	"django.contrib.auth.backends.ModelBackend",
+
+    # allauth specific authentication methods, such as login by e-mail
+	"allauth.account.auth_backends.AuthenticationBackend",
+
+)
+
+SITE_ID = 1
 
 WSGI_APPLICATION = "boutique_ado.wsgi.application"
 
