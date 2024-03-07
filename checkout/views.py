3b764846c5ac29e5b3d61d4e5f5adcad901b8b4b
TCaldato <tcaldato@gmail.com>
2024-03-07 20:13:39 +0000

    Added stripe elements

--- checkout/views.py
+++ checkout/views.py
@@ -14,6 +14,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51OrmtT05x7uMoVaU6pmqC4XdMUDxZo11wphgG6jlNxwZpt3u4CATvqF0EXtiFyxPsixYPC3Uq8n4doG8BT7MLvHR00Tb1m2HwP',
+        'client_secret': 'sk_test_51OrmtT05x7uMoVaUHRgaZgfs7WYtGk7epDk8KyW4F8uqG7mM6SFMW1hPKXhYgNCaFDiTgu9w16Yq8jARC8M1R2oI00dhonEKaD',
     }
 
     return render(request, template, context)
